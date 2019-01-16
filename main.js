b7dc454f5426d1a041988050c2756004e7190686
Miles Flavel <milesflavel@gmail.com>
2019-01-16 22:07:44 +0800

    Add tooltip/title when highlighting objects

--- main.js
+++ main.js
@@ -9,32 +9,33 @@ function init(){
 
   var tv = new SpriteSimple(107, 63, 'img/tv.png');
   var tvscreen = new SpriteAnimated(109, 65, ['img/tv-off.png', 'img/tv-lessthanthree.png', 'img/tv-twitch.png', 'img/tv-instagram.png', 'img/tv-twitter.png', 'img/tv-usa.png']);
-  tvscreen.update = function(){
-    if (scene.mouseover)
-      this.frame = scene.mouseover.tvframe;
-  };
   tvscreen.click = function(){
     navigate('https://www.youtube.com/milesflavel', true);
   };
   tvscreen.tvframe = 1;
+  tvscreen.tooltip = "My Milo Show (YouTube)";
+  scene.tvscreen = tvscreen;
 
   var map = new SpriteSimple(107, 28, 'img/map.png')
   map.click = function(){
     scene.showOverlay('img/overlay-map.png');
   };
   map.tvframe = 5;
+  map.tooltip = "USA Adventures";
 
   var n64 = new SpriteSimple(145, 122, 'img/n64.png');
   n64.click = function(){
     navigate('https://www.twitch.tv/mtmilo', true);
   };
   n64.tvframe = 2;
+  n64.tooltip = "Livestream";
 
   var videosphere = new SpriteSimple(73, 80, 'img/videosphere.png');
   videosphere.click = function(){
     navigate('https://instagram.com/milesflavel', true);
   };
   videosphere.tvframe = 3;
+  videosphere.tooltip = "Instagram";
 
   scene.objects.push(background);
   scene.objects.push(map);
@@ -54,6 +55,11 @@ function draw(timestamp){
   scene.checkMouseover();
   scene.update();
   scene.render();
+
+  if (scene.mouseover){
+    scene.tvscreen.frame = scene.mouseover.tvframe;
+    png_font.drawText(scene.mouseover.tooltip, [(40-scene.mouseover.tooltip.length)/2*8, 0], "white");
+  }
 
   if (scene.showFPS){
     png_font.drawText(Math.floor(1000/(timestamp - lastTimestamp)).toString(), [5, 0], "white");
