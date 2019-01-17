0fed381edaba629f4c6eedc034bdb69881a8cfbf
Miles Flavel <milesflavel@gmail.com>
2019-01-17 22:25:42 +0800

    Add social media icons

--- js/init.js
+++ js/init.js
@@ -2,12 +2,7 @@ function createSceneObjects(){
   var background = new SpriteSimple(0, 0, 'img/background.png');
 
   var tv = new SpriteSimple(107, 63, 'img/tv.png');
-  var tvscreen = new SpriteAnimated(109, 65, ['img/tv-off.png', 'img/tv-lessthanthree.png', 'img/tv-twitch.png', 'img/tv-instagram.png', 'img/tv-twitter.png', 'img/tv-usa.png']);
-  tvscreen.click = function(){
-    navigate('https://www.youtube.com/milesflavel', true);
-  };
-  tvscreen.tvframe = 1;
-  tvscreen.tooltip = "My Milo Show (YouTube)";
+  var tvscreen = new SpriteAnimated(109, 65, ['img/tv-off.png', 'img/tv-lessthanthree.png', 'img/tv-twitch.png', 'img/tv-twitter.png', 'img/tv-instagram.png', 'img/tv-usa.png']);
 
   var map = new SpriteSimple(107, 28, 'img/map.png')
   map.click = function(){
@@ -17,18 +12,36 @@ function createSceneObjects(){
   map.tooltip = "USA Adventures";
 
   var n64 = new SpriteSimple(145, 122, 'img/n64.png');
-  n64.click = function(){
-    navigate('https://www.twitch.tv/mtmilo', true);
-  };
-  n64.tvframe = 2;
-  n64.tooltip = "Livestream";
 
   var videosphere = new SpriteSimple(73, 80, 'img/videosphere.png');
-  videosphere.click = function(){
+
+  var icon1 = new SpriteSimple(41, 33, 'img/icon-youtube.png');
+  icon1.click = function(){
+    navigate('https://www.youtube.com/milesflavel', true);
+  };
+  icon1.tvframe = 1;
+  icon1.tooltip = "My Milo Show (YouTube)";
+
+  var icon2 = new SpriteSimple(77, 33, 'img/icon-twitch.png');
+  icon2.click = function(){
+    navigate('https://www.twitch.tv/mtmilo', true);
+  };
+  icon2.tvframe = 2;
+  icon2.tooltip = "Twitch Stream";
+
+  var icon3 = new SpriteSimple(43, 59, 'img/icon-twitter.png');
+  icon3.click = function(){
+    navigate('https://twitter.com/MilesFlavel', true);
+  };
+  icon3.tvframe = 3;
+  icon3.tooltip = "Milo on Twitter";
+
+  var icon4 = new SpriteSimple(76, 56, 'img/icon-instagram.png');
+  icon4.click = function(){
     navigate('https://instagram.com/milesflavel', true);
   };
-  videosphere.tvframe = 3;
-  videosphere.tooltip = "Instagram";
+  icon4.tvframe = 4;
+  icon4.tooltip = "Instagram";
 
   scene.objects.push(background);
   scene.objects.push(map);
@@ -36,5 +49,11 @@ function createSceneObjects(){
   scene.objects.push(tvscreen);
   scene.objects.push(n64);
   scene.objects.push(videosphere);
+
+  scene.objects.push(icon1);
+  scene.objects.push(icon2);
+  scene.objects.push(icon3);
+  scene.objects.push(icon4);
+
   scene.tvscreen = tvscreen;
 }
